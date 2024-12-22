f84f6cf6629ed663a6e2eeb57e690fb4a5456935
pawanthathsara <pawanthathsara987@gmail.com>
2024-12-22 23:54:46 +0530

    main function added

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "func.h"
+
+int main() {
+    
+	printf("\n\t\t\t    Hostel    Management    System\n\t\t       *****************************************\n\n\t\t\t\t      Menu List\n\t\t-------------------------------------------------------\n\t\t|\t\t\t\t\t\t      |\n");
+    
+	printf("\t\t|    [1]. Add students for hostel\t\t      |\n\t\t|    [2]. View student list\t\t\t      |\n\t\t|    [3]. Remove students\t\t\t      |\n\t\t|    [4]. Edit student data\t\t\t      |\n\t\t|    [5]. Search students\t\t\t      |\n\t\t|    [6]. Available rooms\t\t\t      |\n\t\t|    [7]. Enter less facilities Rooms\t\t      |\n\t\t|    [8]. See less facilities in the rooms\t      |\n\t\t|    [9]. Health status of students\t\t      |\n\t\t|    [10]. Check-in and check-out of student hostels  |\n\t\t|    [11]. End\t\t\t\t\t      |\n\t\t|\t\t\t\t\t\t      |\n\t\t-------------------------------------------------------\n\n");
+	
+	
+	return main_menu();
+	return 0;
+}
